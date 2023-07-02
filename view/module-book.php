501d84f401433ab1d092fcd9c462c5744bcffb88
s-momcilovic <86606868+s-momcilovic@users.noreply.github.com>
2023-07-03 01:11:10 +0200

    Css ispravljen oko single book page i dodata nova sekcija ispod related books

--- view/module-book.php
+++ view/module-book.php
@@ -9,16 +9,16 @@ if (isset($_GET['book_id'])) {
   if ($result->num_rows > 0) {
     // prikaz informacija o knjizi
     $row = $result->fetch_assoc();
-    echo '<div class="book-container">
+    echo '<div class="single-book-wrapper container">
             <div class="book-cover">
-              <img src="' . $row["book_image_path"] . '" alt="" class="book-cover">
+              <img src="' . $row["book_image_path"] . '" alt="">
             </div>
             <div class="book-info">
               <h2>' . $row["book_name"] . '</h2>
               <p>Author: ' . $row["book_author_name"] . '</p>
               <p>Description: ' . $row["book_description"] . '</p>
               <p>Published: ' . $row["book_date_published"] . '</p>
-              <button class="rent-btn">Rent</button>
+              <button class="rent-btn btn">Rent</button>
             </div>
           </div>';
   } else {
@@ -27,6 +27,31 @@ if (isset($_GET['book_id'])) {
 }
 
 ?>
+
+
+<section class="discover-books container">
+    <h3>Related featured books</h3>
+    <div class="books-wrapper">
+      <?php
+        // SQL upit koji prikazuje četiri nasumične knjige
+        $query = "SELECT * FROM book ORDER BY RAND() LIMIT 4";
+        $result = query($query);
+
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            echo '<div class="book-wrapper">
+                    <div class="book-img">
+                      <a href="./index.php?module=book&book_id=' . $row["book_id"] . '"><img src="' . $row["book_image_path"] . '" alt="" class="book-photo"></a>
+                    </div>
+                    <p class="book-title">' . $row["book_name"] . '</p>
+                    <p class="book-author">' . $row["book_author_name"] . '</p>
+                  </div>';
+          }
+        } else {
+          echo "0 results";
+      }?>
+    </div>
+  </section>
 
 
 
