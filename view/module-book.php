1b53c814830c7612455382673c7589adb4a2273d
s-momcilovic <86606868+s-momcilovic@users.noreply.github.com>
2023-07-03 21:33:25 +0200

    Popravka css-a single book page nije bas najbolje

--- view/module-book.php
+++ view/module-book.php
@@ -55,15 +55,3 @@ if (isset($_GET['book_id'])) {
 
 
 
-          <!-- <div class="book-container">
-            <div class="book-cover">
-              <img src="' . $row["book_image_path"] . '" alt="" class="book-cover">
-            </div>
-            <div class="book-info">
-              <h2>' . $row["book_name"] . '</h2>
-              <p>Author: ' . $row["book_author_name"] . '</p>
-              <p>Description: ' . $row["book_description"] . '</p>
-              <p>Published: ' . $row["book_date_published"] . '</p>
-              <button class="rent-btn">Rent</button>
-            </div>
-          </div> -->
