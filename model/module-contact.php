8f2f76125c631b658535e738c2e33b96a4c86be6
ndjerma <djermalearns@gmail.com>
2023-07-03 13:58:53 +0100

    contact stranica zavrsena
    Rade obavestenja pri uspesnom i neuspesnom slanju poruke i pri uspesnom slanju nestaje kontakt forma

--- model/module-contact.php
+++ model/module-contact.php
@@ -1,6 +1,6 @@
 <?php 
-
 $error = [];
+$notification = '';
 $err_msg = ''; //uvodimo da bi smo mogli da stampamo gresku svaku iz niza kao string, inace ide greska
 
   if ($_POST) {
@@ -14,13 +14,14 @@ $err_msg = ''; //uvodimo da bi smo mogli da stampamo gresku svaku iz niza kao st
       $error[] = 'You must enter your email.';
     if ($message == '')
       $error[] = 'You must enter your message.';
+    if(empty($error))
+      $notification = "Your message was sent successfully!";
   }
-
-  if (!empty($error)) {
-    foreach ($error as $err) {  //prolazi $err string kroz $error niz i hvata adektvatnu gresku i upisujemo je u $err_msg
-    $err_msg .= '<div class="alert">' . $err . '</div>';
-    }
-}      
+    if (!empty($error)) {
+      foreach ($error as $err) {  //prolazi $err string kroz $error niz i hvata adektvatnu gresku i upisujemo je u $err_msg
+      $err_msg .= '<div class="alert">' . $err . '</div>';
+      }
+  }
   
   $page_output = [
     'page_title' => 'Contact Us',
