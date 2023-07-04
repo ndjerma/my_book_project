26f2112ffd0ba6a93d7ff570e6b1c902fe2571a5
ndjerma <djermalearns@gmail.com>
2023-07-04 15:43:25 +0100

    admin stranica i css

---
+++ model/module-user.php
@@ -0,0 +1,4 @@
+<?php $page_output = [
+    'page_title' => 'Hello user!',
+    'view' => 'module-user.php'
+];?>
---
+++ view/module-user.php
@@ -0,0 +1,42 @@
+<section class="user-page container">
+    <h1 class="page-title"> <?= $page_output ['page_title'] ?> </h1>
+
+    <h3>These are the books you have rented:</h3>
+    <section class="rented-books-wrapper">
+        <div class="rented-book-wrapper">
+            <div class="rented-book-img">
+                <img src="./public/book-img/1984.jpg" alt="pic">
+            </div>
+            <div class="rented-book-text">
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, facere?</p>
+            </div>
+            <div class="rented-book-btns">
+                <button class="btn btn-remove">
+                    Remove
+                </button>
+                <button class="btn btn-remove">
+                    Remove
+                </button>
+            </div>
+        </div>
+        
+        <div class="rented-book-wrapper">
+            <div class="rented-book-img">
+                <img src="./public/book-img/1984.jpg" alt="pic">
+            </div>
+            <div class="rented-book-text">
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, facere?</p>
+            </div>
+            <div class="rented-book-btns">
+                <button class="btn btn-remove">
+                    Remove
+                </button>
+                <button class="btn btn-remove">
+                    Remove
+                </button>
+            </div>
+        </div>
+    </section>
+</section>
