7a3db141d1fa7806d73e79ab7b2f180498c61c57
ndjerma <djermalearns@gmail.com>
2023-07-04 22:47:12 +0100

    admin greške

--- view/module-admin.php
+++ view/module-admin.php
@@ -1,20 +1,19 @@
 <section class="user-page container">
     <h1 class="page-title"> <?= $page_output ['page_title'] ?> </h1>
 
-    <form class="login-form" method="post" action="./index.php?module=admin.php">
+    <form class="login-form" method="post" action="./index.php?module=admin">
       
-      <?php
-        echo $error_msg;
-      ?>
+      <?php echo $error_msg; ?>
+      <?php display_message(); ?>
 
       <label for="book_name">Naziv knjige:</label>
-      <input type="text" name="book_name" required>      
+      <input type="text" name="book_name" >     
       
       <label for="book_author_name">Ime autora:</label>
-      <input type="text" name="book_author_name" required>
+      <input type="text" name="book_author_name" >
 
       <label for="book_date_published">Datum objavljivanja:</label>
-      <input type="date" name="book_date_published" required>
+      <input type="date" name="book_date_published" >
 
       <label for="book_description">Opis knjige:</label>
       <textarea name="book_description" rows="4" cols="50"></textarea>
