c32875cddbfafd5a1c8d97dbc18767587a49ea06
s-momcilovic <86606868+s-momcilovic@users.noreply.github.com>
2023-07-04 16:19:42 +0200

    stranica user rented books

--- view/module-user.php
+++ view/module-user.php
@@ -1,7 +1,7 @@
 <section class="user-page container">
     <h1 class="page-title"> <?= $page_output ['page_title'] ?> </h1>
 
-    <h3>These are the books you have rented:</h3>
+    <h3 class="rented-page-h3">These are the books you have rented:</h3>
     <section class="rented-books-wrapper">
         <div class="rented-book-wrapper">
             <div class="rented-book-img">
@@ -11,13 +11,16 @@
                 <h4>Lorem ipsum dolor sit amet.</h4>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, facere?</p>
             </div>
+
             <div class="rented-book-btns">
                 <button class="btn btn-remove">
                     Remove
                 </button>
-                <button class="btn btn-remove">
-                    Remove
-                </button>
+
+                <a href="./index.php?module=books" class="btn btn-add">
+                    Add new book
+                </a>
+
             </div>
         </div>
         
@@ -33,9 +36,10 @@
                 <button class="btn btn-remove">
                     Remove
                 </button>
-                <button class="btn btn-remove">
-                    Remove
-                </button>
+
+                <a href="./index.php?module=books" class="btn btn-add">
+                    Add new book
+                </a>
             </div>
         </div>
     </section>
