be971b8a3d77db95f958de762537c43f284e6b7c
s-momcilovic <86606868+s-momcilovic@users.noreply.github.com>
2023-07-06 13:44:04 +0200

    Izlistava useru rentane knjige zavrseno

--- model/module-user.php
+++ model/module-user.php
@@ -1,4 +1,27 @@
-<?php $page_output = [
+<?php
+
+$users_id = $_SESSION['users_id'];
+
+$query = "SELECT book.* FROM rentals JOIN book ON rentals.book_id = book.book_id WHERE rentals.users_id = $users_id";
+$result = query($query);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$page_output = [
     'page_title' => 'Hello, '. $_SESSION['first_name'],
     'view' => 'module-user.php'
 ];?>
--- view/module-user.php
+++ view/module-user.php
@@ -4,7 +4,53 @@
     <h3 class="rented-page-h3">These are the books you have rented:</h3>
     <section class="rented-books-wrapper">
         
-        <div class="rented-book-wrapper">
+<?php 
+    
+if ($result -> num_rows > 0) {
+    while($row = $result->fetch_assoc()){
+        echo '<div class="rented-book-wrapper">
+        <div class="rented-book-img">
+            <img src="' . $row["book_image_path"] . '" alt="pic">
+        </div>
+        <div class="rented-book-text">
+            <h4>' . $row["book_name"] . '</h4>
+            <h3>' . $row["book_author_name"] . '</h3>
+            <p>' . $row["book_description"] . '</p>
+        </div>
+
+        <div class="rented-book-btns">
+            <button class="btn btn-remove">
+                Remove
+            </button>
+
+            <a href="./index.php?module=books" class="btn btn-add">
+                Add new book
+            </a>
+        </div>
+    </div>';
+    }
+} else 
+    echo "0 results"
+    
+    
+    
+    
+    
+    
+    ?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!-- <div class="rented-book-wrapper">
             <div class="rented-book-img">
                 <img src="./public/book-img/1984.jpg" alt="pic">
             </div>
@@ -23,25 +69,8 @@
                 </a>
 
             </div>
-        </div>
+        </div> -->
         
-        <div class="rented-book-wrapper">
-            <div class="rented-book-img">
-                <img src="./public/book-img/1984.jpg" alt="pic">
-            </div>
-            <div class="rented-book-text">
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, facere?</p>
-            </div>
-            <div class="rented-book-btns">
-                <button class="btn btn-remove">
-                    Remove
-                </button>
 
-                <a href="./index.php?module=books" class="btn btn-add">
-                    Add new book
-                </a>
-            </div>
-        </div>
     </section>
 </section>
